7f50f44d3755dbcfd471800effa301c072487338
MKHeidari <mohammadkazemheidari4@gmail.com>
2025-09-24 10:12:38 +0330

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
+// src/index.js
 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+// Create root (React 18+)
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render App
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
